a19b80b27d22d48fbe669b8118444d8dfaa9a8fb
brianwang30 <bwang30@stuy.edu>
2022-12-08 14:03:53 -0500

    a victim of my own doing

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 app.secret_key = "HI" # dummy key CHANGE LATER
 
+#login page is landing page
 @app.route('/', methods=['GET'])
 def login():
-#  if 'username' in session:
-#    return redirect('/home') # go to displ homepage if has cookies
-#  return render_template('login.html')
-  return redirect('/home')
+  if 'username' in session:
+    return redirect('/profile') # go to displ homepage if has cookies
+  return render_template('index.html')
 
 @app.route('/register', methods=['GET'])
 def register():
@@ -20,29 +20,35 @@ def register():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def make_account():
-  #method to create entry into DB
+  #if not unique
   if user_exist(request.form.get('username')):
-    return render_template('registration.html', status='username in use')
+    return render_template('registration.html', status='Pick another Username (In Use)')
+  #if user or pass don't ok
+  #if len(request.form.get('password')) < 8:
+  #  return render_template('registration.html', status='Password is too short')
 
+  #new entry
   create_user(request.form.get('username'), request.form.get('password'))
   session['username'] = request.form['username']
-  return render_template("profile.html")
+  #return render_template("profile.html")
+  return redirect('/profile')
 
 @app.route('/auth', methods=['GET', 'POST'])
 def authenticate():
-  # check if user in db
   # Method to check if login in DB
+  print('hi')
+  print(verify(request.form.get('username'), request.form.get('password')))
   if verify(request.form.get('username'), request.form.get('password')):
     return render_template('index.html', status='Incorrect login info')
+  #return render_template('profile.html')
+  return redirect('/profile')
+
+#prof page
+@app.route('/profile', methods=['GET'])
+def profile():
+  if not session:
+    return redirect('/')
   return render_template('profile.html')
-
-# homepage
-@app.route('/home', methods=['GET'])
-def home():
-  #if not session:
-  #  return redirect('/')
-
-  return render_template('index.html')
 
 
 if __name__ == '__main__':
