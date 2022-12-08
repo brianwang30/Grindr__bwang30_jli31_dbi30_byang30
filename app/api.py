609b5f9897340599ef0b036b46e7f47577b73b3d
Brian Yang <brianyang1206@gmail.com>
2022-12-08 12:46:20 -0500

    Finished anime function

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import random
 
 #PokeApi Dict Lists name/sprite link/type(s)
@@ -20,3 +21,24 @@ def random_poke():
     except:
         None
     info["type"] = [type1, type2]
+
+def random_anime():
+    info = {}
+    rand_index = random.randrange(500)
+    url = "https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=500" # Taking the top ranking anime and selecting a random one (500 is max)
+
+    client_id = "" #Pulling api key
+    try: #check for if text file for key exist
+        with open("keys/MAL_key.txt", "r") as file:
+            api_key = file.read().strip()
+            client_id = api_key
+    except:
+        print("No API key provided.")
+
+    data = json.loads(requests.get(url, headers={"X-MAL-CLIENT-ID": client_id}).text)
+
+    anime_data = data["data"][rand_index]
+    #Name
+    info["name"] = anime_data["node"]["title"]
+    #Image
+    info["image"] = anime_data["node"]["main_picture"]["large"]
